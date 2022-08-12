6e56e3705cd68b87f963286bf6c3df52479d221a
willianchu <willianchu@hotmail.com>
2022-08-11 18:33:21 -0700

    finished testing

--- gradingStudents.test.js
+++ gradingStudents.test.js
@@ -5,17 +5,17 @@ test("if grade = 84 round to 85 (85-84 is less than 3)", () => {
   expect(gradingStudentsCheck(grade)).toStrictEqual([85]);
 });
 
-// test("if grade = 29 do not round (result is still less than 40)", () => {
-//   const grade = [29];
-//   expect(gradingStudentsCheck(grade)).toStrictEqual([29]);
-// });
+test("if grade = 29 do not round (result is still less than 40)", () => {
+  const grade = [29];
+  expect(gradingStudentsCheck(grade)).toStrictEqual([29]);
+});
 
-// test("if grade = 57 do not round (60-57 is 3 or higher)", () => {
-//   const grade = [57];
-//   expect(gradingStudentsCheck(grade).toBe([57]));
-// });
+test("if grade = 57 do not round (60-57 is 3 or higher)", () => {
+  const grade = [57];
+  expect(gradingStudentsCheck(grade)).toStrictEqual([57]);
+});
 
-// test("if grade a full array of grades", () => {
-//   const grade = [73, 67, 38, 33];
-//   expect(gradingStudentsCheck(grade).toBe([75, 67, 40, 33]));
-// });
+test("if grade a full array of grades", () => {
+  const grade = [73, 67, 38, 33];
+  expect(gradingStudentsCheck(grade)).toStrictEqual([75, 67, 40, 33]);
+});
